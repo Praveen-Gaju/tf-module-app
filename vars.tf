7e55e9fb29ad3d714c1fa83dbf673549c4c746b9
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-31 16:47:10 +0530

    updating tf-module-app directory

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "env" {}
+variable "tags" {}
+
+variable "component" {}
+variable "instance_type" {}
+
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "subnets" {}
+
+
+

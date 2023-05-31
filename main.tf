6ca91d9b9b0a73949067b69dc4015d6396926061
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-31 17:00:34 +0530

    updating tf-module-app directory

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_launch_template" "main" {
     )
   }
 
-  user_data = base64decode(templatefile("${path.module}/userdata.sh",{
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",{
     component = var.component
     env       = var.env
   } ))

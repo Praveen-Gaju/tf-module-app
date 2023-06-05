57c2adebfc775d78a7486e1b27be8404c20e93e1
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-05 11:50:58 +0530

    updating tf-module-app directory

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_autoscaling_group" "main" {
   max_size              = var.max_size
   min_size              = var.min_size
   vpc_zone_identifier   = var.subnets
+  target_group_arns     = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
@@ -76,6 +77,26 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  tags       = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
+
+
+#target group of each service
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled               = true
+    healthy_threshold     = 2
+    unhealthy_threshold   = 5
+    interval              = 5
+    timeout               = 4
+  }
   tags       = merge(
     var.tags,
     { Name = "${var.component}-${var.env}" }

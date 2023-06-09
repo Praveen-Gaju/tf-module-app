a2413c7656dcba67a98c3bda084534ad1a11610e
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-09 13:50:08 +0530

    updating tf-module-app directory

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check {
+    path                  = "/health"
     enabled               = true
     healthy_threshold     = 2
     unhealthy_threshold   = 5
